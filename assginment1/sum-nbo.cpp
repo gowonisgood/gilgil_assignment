f1f0e6f8933b5d606d5395262219c8883ac26087
gowonisgood <gowonisgood@pusan.ac.kr>
2025-07-15 15:58:57 +0900

    revised finished

--- assginment1/sum-nbo.cpp
+++ assginment1/sum-nbo.cpp
@@ -5,36 +5,25 @@
 int main(int argc, char* argv[]){
 
     int sum = 0;
-    uint32_t sum32= 0;
 
     for(int i=1; i<argc; i++){
-        FILE *fp = fopen(argv[i], "r");
+        FILE *fp = fopen(argv[i], "rb");
         if(fp == NULL) {
             printf("no file\n");
             return 0;
         }
 
-        uint32_t network_num = 0x00000000;
 
+        uint32_t network_num;
+	size_t ret = fread(&network_num, sizeof(uint32_t) ,1,fp);
 
-        uint8_t ch;
-        for(int i=0;i<4;i++){
-            ch=fgetc(fp);
-            network_num |= ch << i*8;
-            //printf("%02X ",ch); //debug
-        }
-        //printf("\n"); //debug
-        //printf("0x%08x",network_num); //debug
-
-
-        //printf("------\n");
-
-
-        // fread(&network_num, 1, 32, fp); //fail 1
-        // //fscanf(fp, "%u", &network_num); //fail 2
+	if (ret <= 0){
+		printf("fail\n");
+		return 0;
+	}
+ 
 
         uint32_t host_num = ntohl(network_num);
-        //uint32_t sum32 = sum32 + host_num;
         sum += int(host_num);
 
 
@@ -43,7 +32,7 @@ int main(int argc, char* argv[]){
         if(i!=argc-1) printf(" + ");
         else {
             printf("= %d(0x%08x)",sum,sum);
-        }
+        } 
 
         fclose(fp);
 
